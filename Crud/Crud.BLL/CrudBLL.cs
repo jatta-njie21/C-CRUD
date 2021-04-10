bb1703240f2a09ced89d9fbed37f1ca14cf82dc0
jatta-njie21 <laboyjatta@gmail.com>
2021-04-10 14:22:43 +0000

    Added BLL and DTO

---
+++ Crud/Crud.BLL/CrudBLL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Crud.BLL
+{
+	public class Crud
+	{
+		public void GetCrud(CrudDTO crudDto)
+        {
+            //return crud
+        }
+	}
+}
--- Crud/Crud.DTO/CrudDTO.cs
+++ Crud/Crud.DTO/CrudDTO.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections;
+using System.Runtime.Serialization;
+
 namespace Crud.DTO
 {
     [Serializable]
+    //[DataContract]
     
     public class CrudDTO
     {
-        public string UserName { get; set; }
+        //[DataMember(Order = 0)]public string UserName { get; set; }
         public string Password { get; set; }
+
+       
     }
 }
