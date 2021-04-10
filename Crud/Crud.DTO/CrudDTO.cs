4a8f639d8d8f58a2718f97ae2f9447710b1de5e4
jatta-njie21 <laboyjatta@gmail.com>
2021-04-10 14:00:26 +0000

    Second commit

---
+++ Crud/Crud.DTO/CrudDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Crud.DTO
+{
+    [Serializable]
+    
+    public class CrudDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
