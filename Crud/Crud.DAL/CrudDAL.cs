98f21958b4149b3bdeb97f16eea5bc0976ebed1c
jatta-njie21 <laboyjatta@gmail.com>
2021-04-11 22:32:06 +0000

    Added query method in the DAL

--- Crud/Crud.DAL/CrudDAL.cs
+++ Crud/Crud.DAL/CrudDAL.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using System.Data.SQLite;
+
 namespace Crud.DAL
 {
     public class CrudDAL
     {
-        public void GetUser()
+        public void InsertRecord()
         {
-            //get data from sqlite
+            string cs = @"../../../Desktop/CSharp/C-CRUD/Crud";
+
+            //using var con = new SQLiteConnection(cs);
+            //con.Open();
+
+            //using var cmd = new SQLiteCommand(con);
+            //cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Audi',52642)";
+            //cmd.ExecuteNonQuery();
+
+            //return null;
         }
+
+
     }
 }
+
