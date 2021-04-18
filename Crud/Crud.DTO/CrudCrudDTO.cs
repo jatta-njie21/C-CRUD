e1add2933776bedadf4be841a2ae942cf3930b4e
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-04-18 05:40:52 +0000

    #Dev test

---
+++ Crud/Crud.DTO/CrudCrudDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.Serialization;
+
+namespace Crud.DTO
+{
+    [Serializable]
+    [DataContract]
+    
+    public class CrudDTO
+    {
+        [DataMember(Order = 0)]public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
