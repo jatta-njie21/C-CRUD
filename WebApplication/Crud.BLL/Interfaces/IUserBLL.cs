a0a3ebd09a59125ede965271b836a42528211294
Modou Lamin Jatta <laboyjatta@gmail.com>
2021-04-19 22:11:38 +0000

    #Dev - Added UserBLL.cs and IUserBLL.cs

---
+++ WebApplication/Crud.BLL/Interfaces/IUserBLL.cs
@@ -0,0 +1,14 @@
+﻿using Crud.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crud.BLL.Interfaces
+{
+    interface IUserBLL
+    {
+        void InsertUser();
+        UserDTO GetUser(UserDTO user);
+        Boolean DeleteUser();
+    }
+}
---
+++ WebApplication/Crud.BLL/UserBLL.cs
@@ -0,0 +1,24 @@
+﻿using Crud.BLL.Interfaces;
+using Crud.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crud.BLL
+{
+    public class UserBLL : IUserBLL
+    {
+        public void InsertUser()
+        {
+            //insert userdata
+        }
+        public UserDTO GetUser(UserDTO user)
+        {
+            return user;
+        }
+        public Boolean DeleteUser()
+        {
+            return true;
+        }
+    }
+}
